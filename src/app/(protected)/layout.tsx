ef49ed5b5f0c51dcea17822b40dff029b4e6ea1a
locoscowboy <quentin.tristan.pro@gmail.com>
2025-06-01 07:12:13 +0200

    new front bby

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -11,7 +11,7 @@ export default function ProtectedLayout({
   return (
     <div className="flex h-screen">
       <AppSidebar />
-      <main className="flex-1 overflow-y-auto p-6">
+      <main className="flex-1 overflow-y-auto">
         {children}
       </main>
     </div>
